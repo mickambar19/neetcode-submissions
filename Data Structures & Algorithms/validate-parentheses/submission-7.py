e70b183a33fed63f905dc755c8f03508053db286
Alexis Jimenez <alexis.m.jimenez.o@gmail.com>
2026-07-18 19:59:16 -0600

    Add: validate-parentheses - submission-7

---
+++ Data Structures & Algorithms/validate-parentheses/submission-7.py
@@ -0,0 +1,19 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pair = {
+            '}': '{',
+            ')': '(',
+            ']': '[',
+        }
+        
+        stack = []
+        for bracket in s:
+            if bracket not in pair:
+                stack.append(bracket)
+            elif stack and stack[-1] == pair[bracket]:
+                stack.pop()
+            else:
+                return False
+        
+        return not stack
+            
